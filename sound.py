4a2a5fd153bd141091b2f8f21d5784ff8d399986
Ashwath Mittal <68093378+AshwathMittal@users.noreply.github.com>
2021-05-27 22:03:49 +0530

    Create sound.py

---
+++ sound.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+from pydub import AudioSegment
+from pydub.playback import play
+import RPi.GPIO as GPIO
+from time import sleep
+from gpiozero import Button
+from time import sleep
+from datetime import datetime
+
+
+button = Button(3)
+
+
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11,50)
+
+servo1.start(0)
+
+def job(a):
+    while a==0:
+        song = AudioSegment.from_wav("TF001.WAV")
+        print('playing sound using pydub')
+        play(song)
+
+        if button.is_pressed:
+            a = 1;
+while True:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    if current_time=="07:15:00": #alarm time
+        job(0)
+
